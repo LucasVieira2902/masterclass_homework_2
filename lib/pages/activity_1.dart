37688ea9c1d446244c61a394b600e142710fbf30
Lucas Vieira <66380573+LucasVieira2902@users.noreply.github.com>
2022-07-14 22:38:24 -0300

    Finalizado primeiro exercicio

--- lib/pages/activity_1.dart
+++ lib/pages/activity_1.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Activity1 extends StatelessWidget {
+class Activity1 extends StatefulWidget {
   const Activity1({Key? key}) : super(key: key);
 
+  @override
+  State<Activity1> createState() => _Activity1State();
+}
+class _Activity1State extends State<Activity1> {
+  double posX = 20, posY = 20;
+  bool isActive = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Exercício 1'),
+        title: const Text('Desafio do Botão Flutuante'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            bottom: posY,
+            right: posX,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isActive = !isActive;
+                  posX = isActive ? (MediaQuery.of(context).size.width/2 - 60) : 20;
+                  posY = isActive ? (MediaQuery.of(context).size.height - 140) : 20;
+                });
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: isActive ? 120 : 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(isActive ? 0 : 50),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
