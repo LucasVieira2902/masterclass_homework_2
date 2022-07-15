482fb307ebb018cb66d1ff81adc5b65c7c71fcd9
Lucas Vieira <66380573+LucasVieira2902@users.noreply.github.com>
2022-07-15 01:58:43 -0300

    Ajuste de estrutura do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:masterclass_homework_2/pages/activity_1.dart';
-import 'package:masterclass_homework_2/pages/activity_2.dart';
+import 'package:masterclass_homework_2/pages/implicit_animations/activity_1.dart';
+import 'package:masterclass_homework_2/pages/implicit_animations/activity_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,40 +39,76 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Activity1(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Animações Implícitas', style: TextStyle(fontSize: 32)),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Activity1(),
+                    ),
+                  );
+                },
+                child: const Text(
+                    'Exercício 1',
+                    style: TextStyle(fontSize: 32)
                   ),
-                );
-              },
-              child: const Text(
-                  'Exercício 1',
-                  style: TextStyle(fontSize: 32)
-                ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Activity2(),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Activity2(),
+                    ),
+                  );
+                },
+                child: const Text(
+                    'Exercício 2',
+                    style: TextStyle(fontSize: 32)
                   ),
-                );
-              },
-              child: const Text(
-                  'Exercício 2',
-                  style: TextStyle(fontSize: 32)
-                ),
-            ),
-          ],
+              ),
+              const SizedBox(height: 100),
+              const Text('Animações Controladas', style: TextStyle(fontSize: 32)),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Activity1(),
+                    ),
+                  );
+                },
+                child: const Text(
+                    'Exercício 1',
+                    style: TextStyle(fontSize: 32)
+                  ),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Activity2(),
+                    ),
+                  );
+                },
+                child: const Text(
+                    'Exercício 2',
+                    style: TextStyle(fontSize: 32)
+                  ),
+              ),
+            ],
+          ),
         ),
       ),
     );
